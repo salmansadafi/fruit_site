cfbad75b8c59ab4494a0520ee37e0de3fabb73f5
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-22 17:15:57 +0330

    add site id

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'blog',
     'taggit',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SITE_ID = 1
